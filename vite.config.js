4f31c43808b55e3048ad325d44ebd60983be90d1
VeriQ-model <veriqtool@gmail.com>
2026-02-10 22:45:54 +0000

    test: correct name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [react()], //dodanie react plugin do vute
     base: './', //wszystkie assety są ładowane relatywnie, więc strona działa również w Github Pages
     server: {
-        port: 5173,
+        port: 4173,
         host: true,
     },
     build: {
