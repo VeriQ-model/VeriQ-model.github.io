7b747d40876fb6e709f582e1b9a4eb098995a1d2
VeriQ-model <veriqtool@gmail.com>
2026-03-01 00:47:21 +0000

    test visibility v4

--- components/pdf/pdfIndex.js
+++ components/pdf/pdfIndex.js
@@ -5,4 +5,4 @@ export { generatePDF } from './pdfCore.js';
 export { encodeText, formatDate, formatShortDate } from './pdfTextUtils.js'; //kodowanie tekstu, formatowanie dat
 export { getImageDimensions, calculateImageSize, embedImage } from './pdfImageUtils.js'; //wymiary obrazów
 export { checkRadarDataCompleteness, prepareRadarChartData } from './pdfChartGenerator.js'; //sprawdza kompletność danych do wykresu radarowego
-export { hasCommentContent } from './pdfCommentRenderer.js'; //sprawdza czy pdf ma komentarze, które trzeba wyrenderować
+export { hasCommentContent } from './pdfCommentRender.js'; //sprawdza czy pdf ma komentarze, które trzeba wyrenderować
